ff56837cf35129dd28c9c20f4510810f6abc1f50
jokerkwu <south485@daum.net>
2021-05-25 20:46:44 +0900

    포트폴리오 리모델링

--- index.js
+++ index.js
@@ -1,58 +1,91 @@
-$(document).ready(function(e) {
-    var i = 0;
-    var len = $('.slide .textSlide .slide-txt').length;
-    var timeLoop;
-        timeLoop = setInterval(function(){             
-            $('.slide .textSlide .slide-txt').eq(i).css("display","none"); // hide all the li elements
-            i = i + 1;
-            
-            if(i >= len){
-                i = 0;
-            }
-            $('.slide .textSlide .slide-txt').eq(i).css("display","block"); // show the current li
-            if(i==0){
-                $('.mainText').css("background-image","url(img/조현준8_1000.jpg");
-            }else if(i==1){
-                $('.mainText').css("background-image","url(img/조현준9_1000.jpg");
-            }
-            else if(i==2){
-                $('.mainText').css("background-image","url(img/조현준_1000.jpg");
-            }
-            else{
-                $('.mainText').css("background-image","url(img/조현준6_1000.jpg");
-            }
-        }, 3000);
+jQuery(document).ready(function ($) {
+    
+    //프로젝트 모달창
+    const openClickChatBot = document.getElementById("chatBot");
+    const openClickFace = document.getElementById("faceRecognition");
+    const openClickGame = document.getElementById("sicuanCastle");
+    const openClickBoard = document.getElementById("webBoard");
+
+    const modal = document.querySelector(".modalContainer");
+    const overlay = modal.querySelector(".modal_overlay");
+    const closeBtn = modal.querySelector("button");
+
+    const openModal = () => {
+        modal.classList.remove("hidden");
+        document.body.style.overflow = "hidden";
+    };
+
+    const closeModal = () => {
+        modal.classList.add("hidden");
+        document.body.style.overflow = "visible";
+    };
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) {
+            modal.classList.add("hidden");
+            document.body.style.overflow = "visible";
+        }
+    });
+
+    overlay.addEventListener("click", closeModal); 
+    closeBtn.addEventListener("click", closeModal);
+    openClickChatBot.addEventListener("click", openModal);
+    openClickFace.addEventListener("click", openModal);
+    openClickGame.addEventListener("click", openModal);
+    openClickBoard.addEventListener("click", openModal);
+    
+    // Smooth Scrolling
+    
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+    //  Highlight the current section in the navigation bar
+    var sections = $(".section");
+    var navigation_links = $("#nav-wrap a");
+    sections.waypoint({
+            handler: function(event) {
+		   var active_section;
+            active_section = $(this);
+            var active_link = $('#nav-wrap a[href="#' + active_section[0]['element']["id"] + '"]');
+            navigation_links.parent().removeClass("current");
+            active_link.parent().addClass("current");
+		},
+		offset: '0%'
+    })
+
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function() {
+
+        $('header').css({ 'height': $(window).height() });
+        $('body').css({ 'width': $(window).width() })
+   });
+
+
+    // Fade In/Out Primary Navigation
+       $(window).on('scroll', function() {
+
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var nav = $('#nav-wrap');
+
+	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+	      nav.fadeOut('fast');
+	   }
+      else {
+         if (y < h*.20) {
+            nav.removeClass('opaque').fadeIn('fast');
+         }
+         else {
+            nav.addClass('opaque').fadeIn('fast');
+         }
+      }
+	});
 });
-
-
-
-function init(){
-
-//프로젝트 모달창
-const openClickChatBot = document.getElementById("chatBot");
-const openClickFace = document.getElementById("faceRecognition");
-const openClickGame = document.getElementById("sicuanCastle");
-const openClickBoard = document.getElementById("webBoard");
-
-const modal = document.querySelector(".modalContainer");
-const overlay = modal.querySelector(".modal_overlay");
-const closeBtn = modal.querySelector("button");
-
-const openModal = () => {
-  modal.classList.remove("hidden");
-  document.body.style.overflow = "hidden";
-};
-
-const closeModal = () => {
-  modal.classList.add("hidden");
-  document.body.style.overflow = "visible";
-};
-
-overlay.addEventListener("click", closeModal);
-closeBtn.addEventListener("click", closeModal);
-openClickChatBot.addEventListener("click", openModal);
-openClickFace.addEventListener("click", openModal);
-openClickGame.addEventListener("click", openModal);
-openClickBoard.addEventListener("click", openModal);
-
-}
